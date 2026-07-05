88e47f8ab4e7febad8329210c86ae9494442512c
Prince15 <princetandel2006@gmail.com>
2026-07-05 13:47:08 +0530

    feat: update CORS origin configuration and add assistant script to frontend

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "http://localhost:5174"],
         credentials: true,
         methods: ['POST', 'GET']
     }
@@ -36,7 +36,7 @@ app.set("io", io)
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true
 }))
 app.use(express.json())
